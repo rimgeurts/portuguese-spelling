816561f955943b1f3dfb1e9f9d331476aae880dc
rimgeurts <geurts_rim@yahoo.co.uk>
2022-10-26 20:33:19 +0100

    .

--- components/Layout.js
+++ components/Layout.js
@@ -36,12 +36,12 @@ export default function Layout({children}) {
             <div className={'absolute top-[60px] left-0 z-0 w-full  z-0'}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1040 320"><path fill="#f9fafb" fill-opacity="1" d="M0,160L18.5,165.3C36.9,171,74,181,111,197.3C147.7,213,185,235,222,202.7C258.5,171,295,85,332,90.7C369.2,96,406,192,443,218.7C480,245,517,203,554,160C590.8,117,628,75,665,69.3C701.5,64,738,96,775,101.3C812.3,107,849,85,886,69.3C923.1,53,960,43,997,48C1033.8,53,1071,75,1108,122.7C1144.6,171,1182,245,1218,282.7C1255.4,320,1292,320,1329,309.3C1366.2,299,1403,277,1422,266.7L1440,256L1440,0L1421.5,0C1403.1,0,1366,0,1329,0C1292.3,0,1255,0,1218,0C1181.5,0,1145,0,1108,0C1070.8,0,1034,0,997,0C960,0,923,0,886,0C849.2,0,812,0,775,0C738.5,0,702,0,665,0C627.7,0,591,0,554,0C516.9,0,480,0,443,0C406.2,0,369,0,332,0C295.4,0,258,0,222,0C184.6,0,148,0,111,0C73.8,0,37,0,18,0L0,0Z"></path></svg>
             </div>
-            <div className={'absolute bottom-0 left-0 z-0 w-full  z-0'}>
-                <svg className={''} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1040 320">
-                    <path fill="#f9fafb" fill-opacity="1"
-                          d="M0,160L18.5,165.3C36.9,171,74,181,111,197.3C147.7,213,185,235,222,202.7C258.5,171,295,85,332,90.7C369.2,96,406,192,443,218.7C480,245,517,203,554,160C590.8,117,628,75,665,69.3C701.5,64,738,96,775,101.3C812.3,107,849,85,886,69.3C923.1,53,960,43,997,48C1033.8,53,1071,75,1108,122.7C1144.6,171,1182,245,1218,282.7C1255.4,320,1292,320,1329,309.3C1366.2,299,1403,277,1422,266.7L1440,256L1440,320L1421.5,320C1403.1,320,1366,320,1329,320C1292.3,320,1255,320,1218,320C1181.5,320,1145,320,1108,320C1070.8,320,1034,320,997,320C960,320,923,320,886,320C849.2,320,812,320,775,320C738.5,320,702,320,665,320C627.7,320,591,320,554,320C516.9,320,480,320,443,320C406.2,320,369,320,332,320C295.4,320,258,320,222,320C184.6,320,148,320,111,320C73.8,320,37,320,18,320L0,320Z"></path>
-                </svg>
-            </div>
+                {/*<svg className={''} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1040 320">*/}
+                {/*    <path fill="#f9fafb" fill-opacity="1"*/}
+                {/*          d="M0,160L18.5,165.3C36.9,171,74,181,111,197.3C147.7,213,185,235,222,202.7C258.5,171,295,85,332,90.7C369.2,96,406,192,443,218.7C480,245,517,203,554,160C590.8,117,628,75,665,69.3C701.5,64,738,96,775,101.3C812.3,107,849,85,886,69.3C923.1,53,960,43,997,48C1033.8,53,1071,75,1108,122.7C1144.6,171,1182,245,1218,282.7C1255.4,320,1292,320,1329,309.3C1366.2,299,1403,277,1422,266.7L1440,256L1440,320L1421.5,320C1403.1,320,1366,320,1329,320C1292.3,320,1255,320,1218,320C1181.5,320,1145,320,1108,320C1070.8,320,1034,320,997,320C960,320,923,320,886,320C849.2,320,812,320,775,320C738.5,320,702,320,665,320C627.7,320,591,320,554,320C516.9,320,480,320,443,320C406.2,320,369,320,332,320C295.4,320,258,320,222,320C184.6,320,148,320,111,320C73.8,320,37,320,18,320L0,320Z"></path>*/}
+                {/*</svg>*/}
+                <img className={'absolute bottom-0 left-0 z-0 w-full'} src="./background.svg" alt=""/>
+
 
 
             <Disclosure as="nav" className="relative border-b border-gray-200 bg-white ">
@@ -53,12 +53,12 @@ export default function Layout({children}) {
                                     <div className="flex flex-shrink-0 items-center">
                                         <img
                                             className="block h-8 w-auto lg:hidden"
-                                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                            src="https://tailwindui.com/img/logos/mark.svg?color=blue&shade=600"
                                             alt="Your Company"
                                         />
                                         <img
                                             className="hidden h-8 w-auto lg:block"
-                                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                            src="https://tailwindui.com/img/logos/mark.svg?color=blue&shade=600"
                                             alt="Your Company"
                                         />
                                     </div>
@@ -69,7 +69,7 @@ export default function Layout({children}) {
                                                 href={item.href}
                                                 className={classNames(
                                                     item.current
-                                                        ? 'border-indigo-500 text-gray-900'
+                                                        ? 'border-blue-500 text-gray-900'
                                                         : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
                                                     'inline-flex items-center px-1 pt-1 border-b-2 text-lg font-medium'
                                                 )}
@@ -83,7 +83,7 @@ export default function Layout({children}) {
                                 <div className="hidden sm:ml-6 sm:flex sm:items-center">
                                     <button
                                         type="button"
-                                        className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                        className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
                                     >
                                         <span className="sr-only">View notifications</span>
                                         <BellIcon className="h-6 w-6" aria-hidden="true"/>
@@ -93,7 +93,7 @@ export default function Layout({children}) {
                                     <Menu as="div" className="relative ml-3">
                                         <div>
                                             <Menu.Button
-                                                className="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                                                className="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
                                                 <span className="sr-only">Open user menu</span>
                                                 <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt=""/>
                                             </Menu.Button>
@@ -131,7 +131,7 @@ export default function Layout({children}) {
                                 <div className="-mr-2 flex items-center sm:hidden">
                                     {/* Mobile menu button */}
                                     <Disclosure.Button
-                                        className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                                        className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
                                         <span className="sr-only">Open main menu</span>
                                         {open ? (
                                             <XMarkIcon className="block h-6 w-6" aria-hidden="true"/>
--- components/SpellingTest.js
+++ components/SpellingTest.js
@@ -4,6 +4,7 @@ import {CheckCircleIcon} from "@heroicons/react/24/outline";
 import ResultModal from "./ResultModal";
 
 function SpellingTest(props) {
+    const [specialCharacters, setSpecialCharacters] = useState(['á','â','ã','à','ç','é','ê','í','ó','ô','õ','ú']);
     const [words, setWords] = useState(wordList);
     const [openResultsModal, setOpenResultsModal] = useState(false)
     const [currentWordIndex, setCurrentWordIndex] = useState(12);
@@ -55,23 +56,28 @@ function SpellingTest(props) {
         setAnswer('')
     }
 
+    const handleAddSpecialCharacter = (specialCharacter) => {
+        setAnswer((prevAnswer) => prevAnswer.concat(specialCharacter))
+        console.log(specialCharacter)
+    }
+
     return (
         <>
             <ResultModal title={'Correct Answer'} isCorrect={isCorrect} answer={answer} loadNextQuestion={handleLoadNextQuestion} openResultsModal={openResultsModal} correctAnswer={words[currentWordIndex].portguese}/>
             <div className={'pb-10'}>
                 <div className={"flex justify-between p-4 "}>
-                    <div className={`text-xl font-medium px-6 py-3 rounded-md font-medium bg-pink-600 shadow-lg text-white`}>Score {score} out of {totalQuestionsNo}</div>
+                    <div className={`font-medium px-6 py-3 rounded-md font-bold text-2xl bg-pink-100 text-pink-600 border-2 border-pink-600`}>Score: {score}  / {totalQuestionsNo}</div>
                     <button
                         onClick={handleSkipWord}
                         type="button"
-                        className="inline-flex items-center px-6 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        className="inline-flex items-center px-6 py-3 text-base font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
                     >
                         Skip Word
                     </button>
                 </div>
                 <div className="flex flex-col items-center justify-center">
 
-                    <div className="text-6xl font-bold">{words[currentWordIndex].english}</div>
+                    <div className="text-6xl font-bold text-gray-900">{words[currentWordIndex].english}</div>
                 </div>
                 <form onSubmit={handleSubmit} className="mt-10">
                     <div className="flex justify-center ">
@@ -82,16 +88,21 @@ function SpellingTest(props) {
                                 type="text"
                                 name="text"
                                 id="text"
-                                className="block w-full text-center border-2 border-gray-500 border-dashed rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-6xl"
+                                className="block w-full text-gray-900 text-center border-2 border-gray-200 border-dashed rounded-md shadow-sm focus:border-dashed focus:border-blue-500 sm:text-6xl"
                                 onChange={(event) => setAnswer(event.target.value)}
                                 value={answer}
                             />
                         </div>
                     </div>
+                    <div className={'text-4xl flex gap-2 justify-center mt-4 '}>
+                        {specialCharacters.map((specialCharacter, specialCharacterIndex) => {
+                            return(<div onClick={() => handleAddSpecialCharacter(specialCharacter)} className={'w-[50px] p-2 border-2 border-blue-500 rounded-md bg-blue-100 text-blue-500 text-center hover:bg-blue-200 cursor-pointer select-none'} key={specialCharacter}>{specialCharacter}</div>)
+                        })}
+                    </div>
                     <div className="flex justify-center mt-10">
                         <button
                             type="submit"
-                            className="inline-flex items-center px-6 py-3 my-2 text-xl font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            className="inline-flex items-center px-6 py-3 my-2 text-xl font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
                         >
                             Submit
                         </button>
