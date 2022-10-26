d41887a7f5c430f513ae8be297a8572c75b65c5a
rimgeurts <geurts_rim@yahoo.co.uk>
2022-10-26 09:16:47 +0100

    .

---
+++ pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html className="bg-pink-500 min-h-screen w-full">
+      <Head />
+      <body className="min-h-screen w-full">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
